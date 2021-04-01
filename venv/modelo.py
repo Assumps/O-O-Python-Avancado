eba088ca91afecbbca01307d508dfbc2a437f624
Assumps <jrvitor5@gmail.com>
2021-03-31 22:23:49 -0300

    Aprendizado de ducktypes e magic methods do python, aprendendo a fazer a classe atribuir comportamentos com magic methods como ser uma lista ou uma classe geradora de strings e etc

--- venv/modelo.py
+++ venv/modelo.py
@@ -1,30 +1,80 @@
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes}'
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.likes = 0
 
-    def dar_like(self):
-        self.likes += 1
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} - {self._likes}'
 
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.likes = 0
 
-    def dar_like(self):
-        self.likes += 1
+    def __str__(self):
+         return f'{self._nome} - {self.ano} - {self.temporadas} - {self._likes}'
+
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        return self._programas
 
 
+    def __len__(self):
+        return len(self._programas)
 
 
 vingadores = Filme('Vingadores - Guerra Infinita', 2018, 160)
-vingadores.dar_like()
-print(f'nome: {vingadores.nome} - Ano: {vingadores.ano}  - Temporadas: {vingadores.duracao} - Likes: {vingadores.likes}')
-
+tmep = Filme('Todo mundo em panico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
 atlanta = Serie('Atlanta', 2018, 2)
+
+vingadores.dar_like()
+vingadores.dar_like()
+vingadores.dar_like()
+vingadores.dar_like()
+vingadores.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+tmep.dar_like()
+tmep.dar_like()
 atlanta.dar_like()
-print(f'nome: {atlanta.nome} - Ano: {atlanta.ano}  - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
+
+filmes_e_series = [vingadores, atlanta, demolidor,tmep]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
+
+for programa in playlist_fim_de_semana:
+    print(programa)
+
+print(f'Tamanho: {len(playlist_fim_de_semana)}')
